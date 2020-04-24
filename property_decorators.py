63e9d93f38e0611e49d2cf411ecc17a356607c2f
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-24 15:59:11 -0700

    created property decorator sucessfully

--- property_decorators.py
+++ property_decorators.py
@@ -7,14 +7,21 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = first + '.' + last + '@comapny.com'
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    @property
+    def email(self):
+        return '{} {}@email.com'.format(self.first, self.last)
+
 
 emp_1 = Employee('Aaron', 'Glenn', 1000)
+
+emp_1.first = 'Calvin'
 
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname())
+
+# property decorators allow us to define a method, but we can use it like an attribute
